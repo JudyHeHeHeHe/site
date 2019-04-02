1246f6238db7d627a09e13a565590021c6301307
Judy He <jhe@sankynet.com>
2019-04-02 17:32:18 -0400

    add nav bar

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -1,3 +1,30 @@
 import "all.scss";
 
-console.log('Hi there!')
+function openNav(){
+  $('.nav-overlay').fadeIn().addClass('open');
+  $('.nav-burger').addClass('active');
+  if ($('.header').hasClass('is-down')){
+    $('.header').css('height', '100%');
+  }
+}
+
+function closeNav(){
+  $('.nav-overlay').fadeOut().removeClass('open');
+  $('.nav-burger').removeClass('active');
+  if ($('.header').hasClass('dropdown-nav')){
+    $('.header').removeAttr('style');
+  }
+}
+
+$('.nav-button').on('click', function(e){
+  e.preventDefault();
+  if ($('.nav-overlay').is(':hidden')){
+    openNav();
+  } else {
+    closeNav();
+  }
+});
+
+
+
+
